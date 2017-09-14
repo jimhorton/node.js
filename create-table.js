814c606aa3fd9421dbbc7e650a6fdfe96e9ed69f
jimhorton <jim_hrtn@yahoo.com>
2017-09-14 16:14:21 -0700

    Create Table

---
+++ create-table.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "myusername",
+  password: "mypassword",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  /*Create a table named "customers":*/
+  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+});
+
